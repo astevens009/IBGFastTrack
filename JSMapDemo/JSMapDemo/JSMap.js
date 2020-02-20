64df42680234850ab19e235a7fd52d80ff7db162
Andre <astevens009@users.noreply.github.com>
2020-02-20 18:16:07 -0500

    JavaScript Map Demo

---
+++ JSMapDemo/JSMapDemo/JSMap.js
@@ -0,0 +1,11 @@
+﻿let captainMap = new Map();
+
+captainMap.set(1001, 'Morpheus');
+captainMap.set(1002, 'Picard');
+captainMap.set(1003, 'Adama');
+captainMap.set(1004, 'Kirk');
+captainMap.set(1005, 'Sisko');
+
+for (let [k, v] of captainMap) {
+    document.write("<br/>ID: " + k + "<br/>Captain: " + v + "<br/>");
+}
