50a5a9e3d2c2baae813968cfe814e57fc030dc33
Andre <astevens009@users.noreply.github.com>
2020-02-25 05:14:23 -0500

    JS Set assignment
    Demo using JavaScript Set

---
+++ JSSetDemo/JSSetDemo/CaptainSetJS.js
@@ -0,0 +1,13 @@
+﻿var captainSet = new Set();
+
+captainSet.add("Morpheus");
+captainSet.add("Benjamin Sisko");
+captainSet.add("Malcolm Reynolds");
+captainSet.add("Jean-Luc Picard");
+captainSet.add("Bill Adama");
+
+for (let item of captainSet) {
+    document.write("<br/>Captain " + item + "<br/>");
+}
+
+
