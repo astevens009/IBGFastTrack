773e61354f989873196f39b2a7ef64173430e234
Andre <astevens009@users.noreply.github.com>
2020-03-01 15:10:41 -0500

    Tutorial re-write assignment
    Learning by repetition; re-writing tutorial samples at: https://www.geeksforgeeks.org/introduction-object-oriented-programming-javascript/

---
+++ JSOOPDemo2/JSConstructorDemo.js
@@ -0,0 +1,36 @@
+﻿//using a constructor 
+function person(first_name, last_name) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+}
+
+//creating new instances of person object 
+let person1 = new person('Mukul', 'Latiyan');
+let person2 = new person('Rahul', 'Avasthi');
+
+/*
+ *  Original output code....
+ * console.log(person1.first_name);
+ * console.log(`${person2.first_name} ${person2.last_name}`); 
+*/
+
+// Modified output code...
+document.write('<h3>Tutorial Constructor Object:</h3>');
+document.write('<em>Creating new instances of person object</em><br/>');
+document.write(person1.first_name);
+document.write(`<br/>${person2.first_name} ${person2.last_name}`);
+
+document.write("<br/><br/>======================================================<br/>")
+document.write('<h3>Custom Constructor Object:</h3>');
+
+function phone(make, model) {
+    this.make = make;
+    this.model = model;
+}
+
+document.write('<em>Creating new instances of phone object</em><br/>');
+android = new phone('Android', 'Cupcake');
+iPhone = new phone('iPhone', 'Terminator');
+
+document.write('Phone Make: ' + android.make + '<br/>Phone Model: ' + android.model);
+document.write(`<br/><br/>iPhone just got <em>REAL</em>:<br/>Make: ${iPhone.make} <br/>Model: <strong>${iPhone.model}</strong>`)
---
+++ JSOOPDemo2/JSObjectDemo.js
@@ -0,0 +1,49 @@
+﻿//Defining object 
+let person = {
+    first_name: 'Mukul',
+    last_name: 'Latiyan',
+
+    //method 
+    getFunction: function () {
+        return (`The name of the person is  
+          ${person.first_name} ${person.last_name}`);
+    },
+    //object within object 
+    phone_number: {
+        mobile: '12345',
+        landline: '6789'
+    }
+};
+
+/*
+ * Original output code:
+ * console.log(person.getFunction());
+ * console.log(person.phone_number.landline);
+ */
+
+// Modified output code...
+document.write("<h3>Tutorial Object:</h3>");
+document.write("<br/><strong>getFunction result:</strong> <br/>" + person.getFunction());
+document.write("<br/><br/><strong>Object Defined Inside person Object:</strong> <br/><em>person.phone_number.landline = </em>" + person.phone_number.landline);
+
+document.write('<br/><br/>==================================================================<br/><br/>');
+//Custom object
+let starfleetShip = {
+    call_sign: "NCC-1234-A",
+    name: "USS Shango",
+
+    getInfoFunction: function () {
+        return (`Call Sign: ${this.call_sign} <br\>Ship Name: ${this.name}`);
+    },
+
+    // object within an object
+    replicator: {
+        type: 'food',
+        location: 'galley'
+    }
+};
+
+document.write("<h3>Custom Object:</h3>");
+document.write("<br/><strong>getInfoFunction result:</strong> <br/>" + starfleetShip.getInfoFunction());
+document.write("<br/><br/><strong>Custom Object Defined inside starfleetShip object:</strong> <br/><em>starfleetShip.replicator.type = </em>" + starfleetShip.replicator.type);
+
